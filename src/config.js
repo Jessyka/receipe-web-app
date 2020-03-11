cb14e5606f91a89eee5b718dff071e8c47048426
jessykalage <jessyka.l@thoughtworks.com>
2020-03-11 10:52:03 -0300

    fix: update files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-
 const APP_ID = "13823779"
 const APP_KEY = "5f10d3f8439c28de326a0de74774437b"
 export const API_URL = (query = 'chicken') => `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
